228dd33040d5074fd64ae2b1f651cb761d9de7e8
Javier GF <45110776+jgarciafon@users.noreply.github.com>
2018-11-17 00:24:57 +0100

    Create install_OpenCV_dependencies.sh

---
+++ install_OpenCV_dependencies.sh
@@ -0,0 +1,12 @@
+#!/bin/bash/
+
+################################################################################
+################### Install OpenCV dependencies on Ubuntu 16.04 ################
+################################################################################
+sudo apt-get install build-essential cmake pkg-config -y
+sudo apt-get install libjpeg8-dev libtiff5-dev libjasper-dev libpng12-dev -y
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev -y
+sudo apt-get install libxvidcore-dev libx264-dev -y
+sudo apt-get install libgtk-3-dev -y
+sudo apt-get install libatlas-base-dev gfortran -y
+sudo apt-get install python2.7-dev python3.5-dev -y
